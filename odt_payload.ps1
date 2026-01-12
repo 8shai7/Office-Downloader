4810aa880a200c581d6330f4249888a337404cce
Shai Tal <shaital121@gmail.com>
2026-01-12 12:42:25 +0200

    Add Display Level and AcceptEULA properties

--- odt_payload.ps1
+++ odt_payload.ps1
@@ -265,6 +265,7 @@ function Start-OfficeODTInteractive {
 $excludeXml    </Product>
   </Add>
   <Updates Enabled="TRUE" Channel="$channel" />
+   <Display Level="Full" AcceptEULA="FALSE" />
   <Property Name="FORCEAPPSHUTDOWN" Value="TRUE" />
 "@
 
